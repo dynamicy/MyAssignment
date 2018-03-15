62513fce01a2094a7348943e2c2c7347efcdb2ef
dynamicy <fermat.little.theorem@gmail.com>
2018-03-15 10:51:27 +0800

    Fundamental.

--- app/src/main/java/yyworkshop/com/myassignment/MainActivity.kt
+++ app/src/main/java/yyworkshop/com/myassignment/MainActivity.kt
@@ -3,6 +3,13 @@ package yyworkshop.com.myassignment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Title: yyworkshop.com.myassignment.MainActivity<br>
+ * Description: MainActivity
+ *
+ * @author chris
+ * @version 1.0
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
